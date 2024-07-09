71cf0d45784c41ad68f1c501ad2dd1b2be2499e4
pshkltsk <pshkltsk@gmail.com>
2024-07-09 13:48:31 +0300

    Added app colors

---
+++ DayTaskApp/DayTaskApp/Resources/AppColors.swift
@@ -0,0 +1,17 @@
+//
+//  AppColors.swift
+//  DayTaskApp
+//
+//  Created by Pavel Kylitsky on 09/07/2024.
+//
+
+import UIKit
+
+enum AppColors {
+    static let darkBlue = UIColor().hex(0x212832) // bg color
+    static let yellow = UIColor().hex(0xFED36A)
+    static let lightGray = UIColor().hex(0x455A64) // textFields
+    static let gray = UIColor().hex(0x8CAAB9) // texts
+    static let white = UIColor().hex(0xFFFFFF)
+    static let darkGray = UIColor().hex(0x263238) // tab bar
+}
