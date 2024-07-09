10c5657148ec0ef4f79096115c0f52d9db35affe
pshkltsk <pshkltsk@gmail.com>
2024-07-09 13:30:47 +0300

    Added basic project. Deleted storyboard.

---
+++ DayTaskApp/DayTaskApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DayTaskApp
+//
+//  Created by Pavel Kylitsky on 09/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+
+
+}
+
